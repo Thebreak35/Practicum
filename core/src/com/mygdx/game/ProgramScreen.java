07c90172272d12a245db611a22ab10394148b2cc
Thebreak35 <3gthebreak@gmail.com>
2016-11-30 14:37:43 +0700

    add Practicum.java

---
+++ core/src/com/mygdx/game/ProgramScreen.java
@@ -0,0 +1,18 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ScreenAdapter;
+
+public class ProgramScreen extends ScreenAdapter{
+	
+	Practicum prac;
+	
+	public ProgramScreen(Practicum prac) {
+		this.prac = prac;
+	}
+		
+	@Override
+	public void render(float delta) {
+		
+	}
+		
+}

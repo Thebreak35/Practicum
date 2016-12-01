34829d377adeb5a7d614d0b4325a3dfbdb940fcd
Thebreak35 <3gthebreak@gmail.com>
2016-12-01 23:05:27 +0700

    add World , WorldRender

--- core/src/com/mygdx/game/ProgramScreen.java
+++ core/src/com/mygdx/game/ProgramScreen.java
@@ -1,18 +1,29 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
 
 public class ProgramScreen extends ScreenAdapter{
 	
 	Practicum prac;
+	World world;
+	WorldRender worldRender;
 	
 	public ProgramScreen(Practicum prac) {
 		this.prac = prac;
+		world = new World();
+		worldRender = new WorldRender();
 	}
 		
 	@Override
 	public void render(float delta) {
+		world.render();
 		
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		worldRender.update();
 	}
 		
 }

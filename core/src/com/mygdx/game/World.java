93928e1e4827d90a0515194662de6919f2c3dba4
Thebreak35 <3gthebreak@gmail.com>
2016-12-04 14:02:22 +0700

    Add ClickAble button

--- core/src/com/mygdx/game/World.java
+++ core/src/com/mygdx/game/World.java
@@ -1,11 +1,31 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+
 public class World {
+	static Vector2 vector;
+	static Rectangle button_1;
+	static boolean isButton_1_click = false;
+	
 	public World() {
-		
+		vector = new Vector2();
+		button_1 = new Rectangle(50, 100 - 50, 50, 50);
 	}
 	
 	public void update() {
-		
+		vector.x = Gdx.input.getX();
+		vector.y = Gdx.input.getY();
+	}
+	
+	public static boolean isClick() {
+		if(Gdx.input.justTouched()) {
+			if(button_1.contains(vector)) {
+				isButton_1_click = !isButton_1_click;
+				return isButton_1_click;
+			}
+		}
+		return isButton_1_click;
 	}
 }
--- core/src/com/mygdx/game/WorldRender.java
+++ core/src/com/mygdx/game/WorldRender.java
@@ -5,20 +5,37 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class WorldRender {
 	SpriteBatch batch;
-	Texture bg;
+	Texture bg,
+			button_1,
+			button_1_press;
 	
 	public WorldRender() {
 		batch = new SpriteBatch();
 		bg = new Texture("bg.png");
+		button_1 = new Texture("button_1.png");
+		button_1_press = new Texture("button_1_press.png");
 	}
 	
 	public void render() {
 		renderBG();
+		renderButton_1();
 	}
 	
 	private void renderBG() {
 		batch.begin();
 		batch.draw(bg, 0, 0);
 		batch.end();
+	}
+	
+	private void renderButton_1() {
+		if(!World.isClick()) {
+			batch.begin();
+			batch.draw(button_1, 50, 600 - 100);
+			batch.end();
+		} else {
+			batch.begin();
+			batch.draw(button_1_press, 50, 600 - 100);
+			batch.end();
+			}		
 	}
 }

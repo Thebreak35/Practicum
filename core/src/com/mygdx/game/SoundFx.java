628ef12d0619737ac03bdb4aec9c8242cf531f53
Thebreak35 <3gthebreak@gmail.com>
2016-12-04 14:25:58 +0700

    Add Sound

--- core/src/com/mygdx/game/SoundFx.java
+++ core/src/com/mygdx/game/SoundFx.java
@@ -1,12 +1,22 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Sound;
 
 public class SoundFx {
 
-	Sound sound;
+	Sound sound_1;
 	
 	public SoundFx() {
-		
+		sound_1 = Gdx.audio.newSound(Gdx.files.internal("sound_1.mp3"));
+	}
+	
+	public void playSound_1() {
+		sound_1.play();
+		sound_1.loop();
+	}
+	
+	public void stopSound_1() {
+		sound_1.stop();
 	}
 }
--- core/src/com/mygdx/game/World.java
+++ core/src/com/mygdx/game/World.java
@@ -9,20 +9,30 @@ public class World {
 	static Rectangle button_1;
 	static boolean isButton_1_click = false;
 	
+	static SoundFx sound_1;
+	
 	public World() {
 		vector = new Vector2();
 		button_1 = new Rectangle(50, 100 - 50, 50, 50);
+		sound_1 = new SoundFx();
 	}
 	
 	public void update() {
 		vector.x = Gdx.input.getX();
 		vector.y = Gdx.input.getY();
+		
+		System.out.println(isButton_1_click);
 	}
 	
 	public static boolean isClick() {
 		if(Gdx.input.justTouched()) {
 			if(button_1.contains(vector)) {
 				isButton_1_click = !isButton_1_click;
+				if(isButton_1_click == true) {
+					sound_1.playSound_1();
+				} else {
+					sound_1.stopSound_1();
+				}
 				return isButton_1_click;
 			}
 		}
